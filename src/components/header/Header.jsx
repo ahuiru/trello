0b6e830d98ad5f85ac2d7def6cefe05b01eacd2c
ahuiru <zhuangerlangyou@gmail.com>
2022-10-20 01:48:52 +0900

    TaskCard

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "../components/Header.css"
+import "../header/Header.css"
 
 export const Header = () => {
   return (
---
+++ src/components/task/Tasks.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Task } from './Task'
+
+export const Tasks = () => {
+  return (
+    <div>
+      <Task/>
+    </div>
+  )
+}

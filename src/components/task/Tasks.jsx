97cd08ac8c351cd3845e388a4e0df856ac9fa9e5
ahuiru <zhuangerlangyou@gmail.com>
2022-10-21 13:39:29 +0900

    complete drag & drop

--- src/components/task/Tasks.jsx
+++ src/components/task/Tasks.jsx
@@ -2,10 +2,21 @@ import React from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import { Task } from "./Task";
 
+const reorder = (taskList, startIndex, endIndex) => {
+  const outCome = taskList.splice(startIndex, 1)
+  taskList.splice(endIndex, 0, outCome[0])
+}
+
 export const Tasks = ({ taskList, setTaskList, inputText, setInputText }) => {
+
+  const handleDragEnd = (result) => {
+    reorder(taskList, result.source.index, result.destination.index)
+    setTaskList(taskList)
+  }
+
   return (
     <div>
-      <DragDropContext>
+      <DragDropContext onDragEnd={handleDragEnd}>
         <Droppable droppableId="droppable">
           {(provided) => (
             <div {...provided.droppableProps} ref={provided.innerRef}>
